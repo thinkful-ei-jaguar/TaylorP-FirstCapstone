58b736c1048f0bef5b1f27d4cea01f023620a9d9
Taylor Piccarreto <tpic12@gmail.com>
2020-02-24 17:15:50 -0500

    polish

--- src/Recipes/recipeCard.js
+++ src/Recipes/recipeCard.js
@@ -1,20 +1,22 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+//import icons from '../icons/rocksIcon.png'
 import '@fortawesome/free-solid-svg-icons'
 import './recipeCard.css';
 
-/** should user be able to favorite from the recipe search or in extended view? leaning towards extended... search list should just show name and minimal glass pic.
- * anchor tag doesn't seem like the right way to Link these two components, think about extend key value pair then introduce an if statment when rendering in what will be a map function, may get complicated.
- */
-
 class RecipeCard extends Component {
+  renderIcon = (recipe) => {
+    //return <img src={icons} alt='recipe icon' />
+    //console.log(recipe.recipe_img)
+  }
+
   render(){
     const { recipe } = this.props
     return (
       <Link to={`/recipes/${recipe.id}`}>
         <li className='recipe-li'>
           <h2>{recipe.recipe_name}</h2>
-          <span className='fas fa-glass-whiskey'></span>
+          {this.renderIcon(recipe)}
         </li>
       </Link>
     )
